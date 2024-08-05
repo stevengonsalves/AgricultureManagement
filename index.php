aef0bea861c8689be69265c9fad60c43b72063d1
ste <git config --global user.email>
2024-08-05 20:46:04 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,439 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>VegManagment</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="bootstrap\css\bootstrap.min.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="bootstrap\js\bootstrap.min.js"></script>
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="login.css"/>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<style>
+			.footer-distributed{
+    background-color: #292c2f;
+    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+    box-sizing: border-box;
+    width: 100%;
+    text-align: left;
+    font: 16px sans-serif;
+
+    padding: 55px 50px;
+
+}
+
+.footer-distributed .footer-left,
+.footer-distributed .footer-center,
+.footer-distributed .footer-right{
+display: inline-block;
+vertical-align: top;
+}
+
+/* Footer left */
+
+.footer-distributed .footer-left{
+width: 30%;
+}
+
+/* The company logo */
+
+.footer-distributed h3{
+color:  #ffffff;
+font: normal 36px 'Cookie', cursive;
+margin: 0;
+}
+
+.footer-distributed h3 span{
+color:  #5383d3;
+}
+
+/* Footer links */
+
+.footer-distributed .footer-links{
+color:  #ffffff;
+margin: 20px 0 12px;
+padding: 0;
+}
+
+.footer-distributed .footer-links a{
+display:inline-block;
+line-height: 1.8;
+text-decoration: none;
+color:  inherit;
+}
+
+.footer-distributed .footer-company-name{
+color:  #8f9296;
+font-size: 14px;
+font-weight: normal;
+margin: 0;
+}
+
+/* Footer Center */
+
+.footer-distributed .footer-center{
+width: 45%;
+}
+
+.footer-distributed .footer-center i{
+background-color:  #33383b;
+color: #ffffff;
+font-size: 25px;
+width: 38px;
+height: 38px;
+border-radius: 50%;
+text-align: center;
+line-height: 42px;
+margin: 10px 15px;
+vertical-align: middle;
+}
+
+.footer-distributed .footer-center i.fa-envelope{
+font-size: 17px;
+line-height: 38px;
+}
+
+.footer-distributed .footer-center p{
+display: inline-block;
+color: #ffffff;
+vertical-align: middle;
+margin:0;
+}
+
+.footer-distributed .footer-center p span{
+display:block;
+font-weight: normal;
+font-size:14px;
+line-height:2;
+}
+
+.footer-distributed .footer-center p a{
+color:  #5383d3;
+text-decoration: none;;
+}
+
+
+/* Footer Right */
+
+.footer-distributed .footer-right{
+width: 20%;
+}
+
+.footer-distributed .footer-company-about{
+line-height: 20px;
+color:  #92999f;
+font-size: 13px;
+font-weight: normal;
+margin: 0;
+}
+
+.footer-distributed .footer-company-about span{
+display: block;
+color:  #ffffff;
+font-size: 14px;
+font-weight: bold;
+margin-bottom: 20px;
+}
+
+.footer-distributed .footer-icons{
+margin-top: 25px;
+}
+
+.footer-distributed .footer-icons a{
+display: inline-block;
+width: 35px;
+height: 35px;
+cursor: pointer;
+background-color:  #33383b;
+border-radius: 2px;
+
+font-size: 20px;
+color: #ffffff;
+text-align: center;
+line-height: 35px;
+
+margin-right: 3px;
+margin-bottom: 5px;
+}
+
+/* If you don't want the footer to be responsive, remove these media queries */
+
+@media (max-width: 880px) {
+
+.footer-distributed{
+font: bold 14px sans-serif;
+}
+
+.footer-distributed .footer-left,
+.footer-distributed .footer-center,
+.footer-distributed .footer-right{
+display: block;
+width: 100%;
+margin-bottom: 40px;
+text-align: center;
+}
+
+.footer-distributed .footer-center i{
+margin-left: 0;
+}
+
+}
+
+
+
+.destacados{
+padding: 70px 0;
+text-align: center;
+
+}
+
+.box1 {
+/* See "NOTE 3" */
+position:relative;
+z-index:1;
+width:20%;
+padding:5px;
+
+border: 1px solid rgb(200, 200, 200);
+box-shadow: rgba(0, 0, 0, 0.1) 0px 5px 5px 2px;
+background:  #E4C054;
+border-radius: 4px;
+}
+		</style>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+
+	<?php
+		require 'menu.php';
+	?>
+
+		<!-- Banner -->
+			<section id="banner" class="wrapper">
+				<div class="container" style="margin-top:-100px">
+				<h2 style="font-weight: bold;" >VegManagment</h2>
+				<p style="font-weight: bold ;" >Your Product Our Market</p>
+				<br><br>
+				<center>
+					<div>
+						<div class="6u 12u$(xsmall)">
+							<button class="button fit" onclick="document.getElementById('id01').style.display='block'" style="width:auto;background-color:green; border-radius:25px">LOGIN</button>
+						</div>
+
+						<div class="6u 12u$(xsmall)">
+							<button class="button fit" onclick="document.getElementById('id02').style.display='block'" style="width:auto;background-color:green; border-radius:25px">REGISTER</button>
+						</div>
+					</div>
+				</center>
+
+
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1 align-center">
+				<div class="container" >
+					<header>
+						<h2 style="font-weight: bold;">VegManagment</h2>
+						<p>Explore the new way of trading...</p>
+					</header>
+					<div class="row 200%">
+						<section class="4u 12u$(small)">
+							<i class="icon big rounded fa-clock-o"></i>
+							<p>Digital Market</p>
+						</section>
+						<section class="4u 12u$(small)">
+							<i class="icon big rounded fa-comments"></i>
+							<p>Agro-Blog</p>
+						</section>
+						<section class="4u$ 12u$(small)">
+							<i class="icon big rounded fa-user"></i>
+							<p>Register with us</p>
+						</section>
+					</div>
+				</div>
+			</section>
+
+
+		<!-- Footer -->
+		<footer class="footer-distributed" style="background-color:black" id="aboutUs">
+		<center>
+			<h1 style="font: 35px calibri;">About Us</h1>
+		</center>
+		<div class="footer-left">
+			<h3 style="font-family: 'Times New Roman', cursive;">VegManagment &copy; </h3>
+			<div class="logo">
+				<a href="index.php"><img src="images/logo.png" width="200px"></a>
+			</div>
+			<br />
+			<p style="font-size:20px;color:white">Your product Our market !!!</p>
+			<br />
+		</div>
+
+		<div class="footer-center">
+			<div>
+				<i class="fa fa-map-marker"></i>
+				<p style="font-size:20px">Agro Culture Fam<span>Vormir</span></p>
+			</div>
+			<div>
+				<i class="fa fa-phone"></i>
+				<p style="font-size:20px">123456789</p>
+			</div>
+			<div>
+				<i class="fa fa-envelope"></i>
+				<p style="font-size:20px"><a href="mailto:VegManagent@gmail.com" style="color:white">demo@demo.com</a></p>
+			</div>
+		</div>
+
+		<div class="footer-right">
+			<p class="footer-company-about" style="color:white">
+				<span style="font-size:20px"><b>About VegManagment</b></span>
+				VegManagment is e-commerce trading platform for grains & grocerries...
+			</p>
+			<div class="footer-icons">
+				<a  href="#"><i style="margin-left: 0;margin-top:5px;"class="fa fa-facebook"></i></a>
+				<a href="#"><i style="margin-left: 0;margin-top:5px" class="fa fa-instagram"></i></a>
+				<a href="#"><i style="margin-left: 0;margin-top:5px" class="fa fa-youtube"></i></a>
+			</div>
+		</div>
+
+	</footer>
+
+
+			<div id="id01" class="modal">
+
+  <form class="modal-content animate" action="Login/login.php" method='POST'>
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+
+    <div class="container">
+    <h3>Login</h3>
+							<form method="post" action="Login/login.php">
+								<div class="row uniform 50%">
+									<div class="7u$">
+										<input type="text" name="uname" id="uname" value="" placeholder="UserName" style="width:80%" required/>
+									</div>
+									<div class="7u$">
+										<input type="password" name="pass" id="pass" value="" placeholder="Password" style="width:80%" required/>
+									</div>
+								</div>
+									
+									<center>
+									<div class="row uniform">
+										<div class="7u 12u$(small)">
+											<input type="submit" value="Login" />
+										</div>
+									</div>
+									</center>
+								</div>
+							</form>
+						</section>
+</div>
+    </div>
+    </div>
+  </form>
+</div>
+
+
+<div id="id02" class="modal">
+
+  <form class="modal-content animate" action="Login/signUp.php" method='POST'>
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+
+    <div class="container">
+
+<section>
+							<h3>SignUp</h3>
+							<form method="post" action="Login/signUp.php">
+								<center>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="name" id="name" value="" placeholder="Name" required/>
+									</div>
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="uname" id="uname" value="" placeholder="UserName" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="mobile" id="mobile" value="" placeholder="Mobile Number" required/>
+									</div>
+
+									<div class="3u 12u$(xsmall)">
+										<input type="email" name="email" id="email" value="" placeholder="Email" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+			                            <input type="password" name="password" id="password" value="" placeholder="Password" required/>
+			                        </div>
+			                        <div class="3u 12u$(xsmall)">
+			                            <input type="password" name="pass" id="pass" value="" placeholder="Retype Password" required/>
+			                        </div>
+								</div>
+								<div class="row uniform">
+									<div class="6u 12u$(xsmall)">
+										<input type="text" name="addr" id="addr" value="" placeholder="Address" style="width:80%" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(small)">
+										<input type="submit" value="Submit" name="submit" class="special" /></li>
+									</div>
+                                      </center>
+									<div class="3u 12u$(small)">
+										<input type="reset" value="Reset" name="reset"/></li>
+									</div>
+								</div>
+							</center>
+							</form>
+						</section>
+
+    </div>
+    </div>
+  </form>
+</div>
+
+
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal1= document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+
+</script>
+
+
+	</body>
+</html>
